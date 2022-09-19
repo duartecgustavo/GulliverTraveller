134de6863c8991960126bdb52e09a479716e174f
ClaudemirRomanoDosSantos-GustavoCastanho-LeilaRibeiro-RenataAntoniassi-ViniciusMello <duartecgustavo@outlook.com>
2022-09-18 22:12:07 -0300

    package.json

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + "/src//index.html")
 })
 
 //localhost:8080
