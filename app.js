4a701a6de90cf2442e6c9cc6e8e5f26f424ec266
ClaudemirRomanoDosSantos-GustavoCastanho-LeilaRibeiro-RenataAntoniassi-ViniciusMello <duartecgustavo@outlook.com>
2022-09-18 22:23:16 -0300

    package.json

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-var path = require('path'); s
+var path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/src//index.html")
 })
-app.use(express.static(path.join(__dirname, 'src/styles/global.css')));
+app.use(express.static(path.join(__dirname, '/src/styles')));
 
 //localhost:8080
 app.listen(port, () => {
