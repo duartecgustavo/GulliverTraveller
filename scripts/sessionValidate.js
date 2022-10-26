133ad973d85f43bb84f4e9a159ba362d8b5a07d7
Gustavo Castanho Duarte <gustavo.duarte@actdigital.com>
2022-10-26 19:26:49 -0300

    ajustada area de login

--- scripts/sessionValidate.js
+++ scripts/sessionValidate.js
@@ -8,35 +8,48 @@ const btnLogin = document.getElementById("btnLogin");
 localStorage.setItem("acesso", false);
 
 btnLogin.addEventListener("click", async () => {
-
   const myItens = getLogins();
 
-  myItens.then((doc) => doc.forEach((el) => passLogin(el.data().email, el.data().senha)));
-
+  myItens.then((doc) =>
+    doc.forEach((el) => passLogin(el.data().email, el.data().senha))
+  );
 
   function passLogin(email, senha) {
     if (email === txtEmail.value && senha === txtSenha.value) {
-      window.location.href = 'index.html';
+      window.location.href = "index.html";
       localStorage.setItem("acesso", true);
     } else {
-      alert("Login invalido")
+      alert("Login invalido");
     }
   }
 });
 
-
 // FIND USER
 
-const contentLogins = document.querySelector(".contentLogins")
+const contentLogins = document.querySelector(".contentLogins");
 
 export function insertInContent(email, senha) {
-
   const element = `
       <p>Login: ${email}</p>
       <p>Senha: ${senha}</p>
-  `
-  contentLogins.insertAdjacentHTML("beforeend", element)
+  `;
+  contentLogins.insertAdjacentHTML("beforeend", element);
 }
 
 const myItens = getLogins();
-myItens.then((doc) => doc.forEach((el) => insertInContent(el.data().email, el.data().senha)));
+myItens.then((doc) =>
+  doc.forEach((el) => insertInContent(el.data().email, el.data().senha))
+);
+
+// Visualizar senha
+
+const btnVisualizar = document.getElementById("btnVisualizar");
+const imputPassword = document.getElementById("password_1");
+
+btnVisualizar.addEventListener("click", () => {
+  if (imputPassword.type == "password") {
+    imputPassword.type = "text";
+  } else {
+    imputPassword.type = "password";
+  }
+});
